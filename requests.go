e186e8a38df5061900c03277450fe8380373a660
Sergey <serge@cheebo.me>
2017-09-21 03:56:56 +0500

    added listoptions structure for requests with pagination

---
+++ requests.go
@@ -0,0 +1,10 @@
+package rest
+
+
+type ListOptions struct {
+	// For paginated result sets, page of results to retrieve.
+	Page int `url:"page,omitempty"`
+
+	// For paginated result sets, the number of results to include per page.
+	PerPage int `url:"per_page,omitempty"`
+}
